d4f05b72a7c7b23f74f9bf293618fbcd6921d64f
hnh12358 <hnh12358@gmail.com>
2010-09-17 09:01:52 -0400

    Added Merthin.FSharp.Tests project

---
+++ Merthin.FSharp.Tests/FSharpCodesTest.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Merthin.FSharp.Tests
+{
+    [TestFixture]
+    public class FSharpCodesTest
+    {
+        [Test]
+        public void methodIdentifier_CombinedOperators_Correct()
+        {
+
+        }
+    }
+
+    internal class MethodContainer
+    {
+        // Methods
+        public static int operator +(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_AdditionAssignment(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_Append(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator &(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator |(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_ComposeLeft(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_ComposeRight(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_Concatenate(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_Dereference(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator /(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_DivisionAssignment(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_Dynamic(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_DynamicAssignment(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator ==(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator ^(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_GreaterLessPlusMinusMultiplyDivideEqualsTwiddlePercentDotAmpBarAtHatBangQmark(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator >(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator >=(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator <<(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator <(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator <=(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_LogicalNot(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator %(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator *(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_MultiplyAssignment(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_PipeLeft(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_PipeRight(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_Quotation(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_QuotationUntyped(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_Range(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_RangeStep(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator >>(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int operator -(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+
+        public static int op_SubtractionAssignment(MethodContainer a, MethodContainer b)
+        {
+            return 4;
+        }
+    }
+
+}
