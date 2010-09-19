27c1dbd1aa17aa55ed127ab5d71d6ea52fd69bdb
hnh12358 <hnh12358@gmail.com>
2010-09-19 02:18:16 -0400

    Add support for Tuples, Options and generic types on FSharpCodes module.

---
+++ Merthin.Extensions/System.String.fs
@@ -0,0 +1,19 @@
+﻿namespace Merthin.Extensions
+open System
+open System.Drawing
+open System.Runtime.CompilerServices
+
+[<Extension>]
+[<AutoOpen>]
+module SystemString =
+
+    [<Extension>]
+    [<CompiledNameAttribute("EllipsisIfNeeded")>]
+    let _EllipsisIfNeeded(source : String,length) =
+        if source.Length > length then
+           source.Substring(0,length - 3) + "..."
+        else 
+            source
+
+    type System.String with
+        member this.EllipsisIfNeeded(length) = _EllipsisIfNeeded(this,length)
--- Merthin.FSharp.Tests/FSharpCodesTest.cs
+++ Merthin.FSharp.Tests/FSharpCodesTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using NUnit.Framework;
 
@@ -9,185 +10,114 @@ namespace Merthin.FSharp.Tests
     [TestFixture]
     public class FSharpCodesTest
     {
-        [Test]
-        public void methodIdentifier_CombinedOperators_Correct()
-        {
+        #region MethodIdentifier
 
+        [Test]
+        public void MethodIdentifier_CombinedOperators_Correct()
+        {
+            var method = typeof(MethodContainer<>).GetMethods(BindingFlags.Static | BindingFlags.Public)[0];
+            var actual = FSharpCodes.MethodIdentifier(method);
+            const string expected = "( ><+-*/=~%.&|@^!? )";
+            Assert.AreEqual(expected, actual);
         }
+
+        #endregion
+
+        #region TypeIdentifier
+
+        [Test]
+        public void TypeIdentifier_GenericTypesClosed_Correct()
+        {
+            var nullableIntTypeCode = FSharpCodes.TypeIdentifier(typeof(Nullable<Int32>));
+            Assert.AreEqual("Nullable<int>", nullableIntTypeCode);
+
+            var dictionaryType = new Dictionary<String, Dictionary<Int32, Double>>().GetType();
+            var dictionaryTypeCode = FSharpCodes.TypeIdentifier(dictionaryType);
+            Assert.AreEqual("Dictionary<string,Dictionary<int,double>>", dictionaryTypeCode);
+        }
+
+        [Test]
+        public void TypeIdentifier_GenericTypesOpen_Correct()
+        {
+            var genericOpenType1 = FSharpCodes.TypeIdentifier(typeof(MethodContainer<>));
+            Assert.AreEqual("MethodContainer<'T>", genericOpenType1);
+
+            var genericOpenType2 = FSharpCodes.TypeIdentifier(typeof(Dictionary<,>));
+            Assert.AreEqual("Dictionary<'TKey,'TValue>", genericOpenType2);
+        }
+
+        [Test]
+        public void TypeIdentifier_Tuples_Correct()
+        {
+            var tuple1Type = Tuple.Create(1).GetType();
+            var tuple1TypeCode = FSharpCodes.TypeIdentifier(tuple1Type);
+            Assert.AreEqual("int",tuple1TypeCode);
+
+            var tuple2Type = Tuple.Create(1, 2).GetType();
+            var tuple2TypeCode = FSharpCodes.TypeIdentifier(tuple2Type);
+            Assert.AreEqual("int * int", tuple2TypeCode);
+
+            var tuple3Type = Tuple.Create(1, 2, 3).GetType();
+            var tuple3TypeCode = FSharpCodes.TypeIdentifier(tuple3Type);
+            Assert.AreEqual("int * int * int", tuple3TypeCode);
+
+            var tuple4Type = Tuple.Create(1, 2, 3, 4).GetType();
+            var tuple4TypeCode = FSharpCodes.TypeIdentifier(tuple4Type);
+            Assert.AreEqual("int * int * int * int", tuple4TypeCode);
+
+            var tuple5Type = Tuple.Create(1, 2, 3, 4, 5).GetType();
+            var tuple5TypeCode = FSharpCodes.TypeIdentifier(tuple5Type);
+            Assert.AreEqual("int * int * int * int * int", tuple5TypeCode);
+
+            var tuple6Type = Tuple.Create(1, 2, 3, 4, 5, 6).GetType();
+            var tuple6TypeCode = FSharpCodes.TypeIdentifier(tuple6Type);
+            Assert.AreEqual("int * int * int * int * int * int", tuple6TypeCode);
+
+            var tuple7Type = Tuple.Create(1, 2, 3, 4, 5, 6, 7).GetType();
+            var tuple7TypeCode = FSharpCodes.TypeIdentifier(tuple7Type);
+            Assert.AreEqual("int * int * int * int * int * int * int", tuple7TypeCode);
+
+            var tuple8Type = Tuple.Create(1, 2, 3, 4, 5, 6, 7, Tuple.Create(8)).GetType();
+            var tuple8TypeCode = FSharpCodes.TypeIdentifier(tuple8Type);
+            Assert.AreEqual("int * int * int * int * int * int * int * int", tuple8TypeCode);
+        }
+
+        [Test]
+        public void TypeIdentifier_PrimitiveDataTypes_Correct()
+        {
+            var pairs = new[]
+                            {
+                                Tuple.Create(typeof (Byte), "byte"),
+                                Tuple.Create(typeof (Boolean), "bool"),
+                                Tuple.Create(typeof (SByte), "sbyte"),
+                                Tuple.Create(typeof (Int16), "int16"),
+                                Tuple.Create(typeof (UInt16), "uint16"),
+                                Tuple.Create(typeof (Int32), "int"),
+                                Tuple.Create(typeof (UInt32), "uint32"),
+                                Tuple.Create(typeof (Int64), "int64"),
+                                Tuple.Create(typeof (UInt64), "uint64"),
+                                Tuple.Create(typeof (UIntPtr), "unativeint"),
+                                Tuple.Create(typeof (Char), "char"),
+                                Tuple.Create(typeof (String), "string"),
+                                Tuple.Create(typeof (Decimal), "decimal"),
+                                Tuple.Create(typeof (void), "unit"),
+                                Tuple.Create(typeof (Single), "float32"),
+                                Tuple.Create(typeof (Double), "double")
+                            };
+            foreach(var pair in pairs)
+                Assert.AreEqual(pair.Item2,FSharpCodes.TypeIdentifier(pair.Item1));
+        }
+
+        #endregion
     }
 
-    internal class MethodContainer
+    internal class MethodContainer<T>
     {
-        // Methods
-        public static int operator +(MethodContainer a, MethodContainer b)
+        public static int op_GreaterLessPlusMinusMultiplyDivideEqualsTwiddlePercentDotAmpBarAtHatBangQmark(MethodContainer<T> a, MethodContainer<T> b)
         {
             return 4;
         }
 
-        public static int op_AdditionAssignment(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_Append(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator &(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator |(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_ComposeLeft(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_ComposeRight(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_Concatenate(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_Dereference(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator /(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_DivisionAssignment(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_Dynamic(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_DynamicAssignment(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator ==(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator ^(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_GreaterLessPlusMinusMultiplyDivideEqualsTwiddlePercentDotAmpBarAtHatBangQmark(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator >(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator >=(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator <<(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator <(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator <=(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_LogicalNot(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator %(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator *(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_MultiplyAssignment(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_PipeLeft(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_PipeRight(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_Quotation(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_QuotationUntyped(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_Range(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_RangeStep(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator >>(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int operator -(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
-
-        public static int op_SubtractionAssignment(MethodContainer a, MethodContainer b)
-        {
-            return 4;
-        }
     }
 
 }
