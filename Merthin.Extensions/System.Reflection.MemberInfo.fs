59e62377391c2ea89cac86d54e98ed48e87bdb5d
hnh12358 <hnh12358@gmail.com>
2010-10-17 23:14:04 -0400

    Added extension method to work more easily with reflection.

---
+++ Merthin.Extensions/System.Reflection.MemberInfo.fs
@@ -0,0 +1,19 @@
+﻿namespace Merthin.Extensions
+open System
+open System.Reflection
+open System.Runtime.CompilerServices
+
+open Merthin.FSharp
+
+[<Extension>]
+[<AutoOpen>]
+module SystemReflectionMemberInfo =
+    
+    [<Extension>]
+    let HasAttribute<'a when 'a :> Attribute>(source : MemberInfo) =
+        requires (source <> null) "Null Argument"
+        source.GetCustomAttributes(true)
+        |> Seq.exists(fun x -> x.GetType() = typeof<'a>)
+
+    type System.Reflection.MemberInfo with
+        member this.HasAttribute<'a when 'a :> Attribute>() = HasAttribute<'a>(this) 
